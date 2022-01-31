4c888c2da4e58bc8ec8b0e31dfcce3acea5604e2
Raja-AR-007 <92211139+Raja-AR-007@users.noreply.github.com>
2022-01-31 14:21:39 +0530

    Add files via upload

---
+++ 12-cmd promt user input as 'argv'.py
@@ -0,0 +1,9 @@
+#                         Command Promt user input using folder
+# Save this code in Desktop
+# Run this code as folder in cmd promt
+import sys
+
+x = int(sys.argv[1]) #'argv' means argument values
+y = int(sys.argv[2])
+z = x + y
+print(z)
